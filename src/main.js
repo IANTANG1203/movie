5c2f98fe398af944ba1f4f80eb05a8d06a981f84
Bestian Tang <besitan@gmail.com>
2016-10-07 19:57:04 +0800

    install vue-resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 import App from './App'
 import Home from './components/Home'
