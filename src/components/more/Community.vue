0d016f97dfff62a55eec835c665a3809f14f9a57
Bestian Tang 唐宗浩 <besitan@gmail.com>
2016-10-15 18:15:25 +0800

    add container

--- src/components/more/Community.vue
+++ src/components/more/Community.vue
@@ -1,13 +1,14 @@
 <template lang="jade">
   .hi
-    h1 {{ msg }}
-    h3.ui.dividing.header 國際社群
-    .ui.bulleted.list
-      a.item(href='https://vuejs.org/guide/join.html', target="_blank") Vue Community
+    .ui.container
+      h1 {{ msg }}
+      h3.ui.dividing.header 國際社群
+      .ui.bulleted.list
+        a.item(href='https://vuejs.org/guide/join.html', target="_blank") Vue Community
 
-    h3.ui.dividing.header 台灣社群
-    .ui.bulleted.list
-      a.item(href='https://www.facebook.com/groups/vuejs.tw/?fref=ts', target="_blank") Vue.js 台灣
+      h3.ui.dividing.header 台灣社群
+      .ui.bulleted.list
+        a.item(href='https://www.facebook.com/groups/vuejs.tw/?fref=ts', target="_blank") Vue.js 台灣
 </template>
 
 <script>
