fef0a33450f33f5a4e1d64b9917fac007a45ad5e
Bestian Tang <besitan@gmail.com>
2016-10-21 16:46:34 +0800

    article init

--- src/components/more/Basic.vue
+++ src/components/more/Basic.vue
@@ -3,6 +3,27 @@
     h1 {{ msg }}
     
     .ui.container
+      .ui.segment
+        p 起初，在瀏覽器端不能跑程式。
+        p 接著，有
+          a(href = "https://zh.wikipedia.org/zh-tw/JavaScript") JavaScript(JS/ES)
+          | 但很慢。
+        p 當
+          a(href = "https://zh.wikipedia.org/zh-tw/JavaScript") JS
+          | 變快後，各種功能開始可以往前端移動，節省後端的運算量。
+        p 接著，讓JS操作簡化，強調「寫更少，做更多」的
+          a(href="https://zh.wikipedia.org/zh-tw/JQuery") jQuery
+          | 函式庫，主導了泛用開發工具的生態，據
+          a(href="https://zh.wikipedia.org/wiki/JQuery") 維基百科
+          | ，全球前10,000個存取最高的網站中，有65%網站都用了
+          a(href="https://zh.wikipedia.org/zh-tw/JQuery") jQuery
+          |。
+        p 但故事還沒有結束。
+        p 在前端的部份，框架(framework)試圖處理超過jQuery所做的事：它們直接給設計師一個架構與規格，讓常見的工作用盡量少的力氣達成，並且讓往上蓋大的時候程式還能保有容易理解維護的結構。
+        p 不同框架有不同的規格，背後的引擎不同，表面上設計師寫的語法也很不一樣。
+        p 在許多框架被研發之後，AngularJS這個框架一度造成震撼性的效果。因其資料綁定、相依性嵌入等特性，讓書寫方便的同時，也讓建立、分享與安裝模組也很方便。
+        p 大量的Angular模組被研發出來，放上ooxx平台。
+        p test...
       h3.ui.dividing.header 語言
       .ui.bulleted.list
         .item VallinaJS: 基本的JS
@@ -40,6 +61,13 @@ $c: #42b983;
 
 h1, h3 {
   color: $c;
+}
+
+p {
+  font-size: 0.75rem;
+  text-align: left;
+  line-height: 160%;
+  margin-bottom: 1em;
 }
 
 .ui.bulleted.list {
