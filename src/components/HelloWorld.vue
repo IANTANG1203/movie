703ab5161dccac60749a7fa7d2d83abe0b8c3bdd
IANTANG1203 <iant515gc@gmail.com>
2019-03-09 10:21:56 +0800

    換圖

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <img src="https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg">
+    <img src="../assets/123">
   </div>
 </template>
 
@@ -11,7 +11,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '我的影評'
+      msg: "我的影評"
     }
   }
 }
